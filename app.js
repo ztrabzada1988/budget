46dacd55a6c43f788c1ce173330049d53eeb2c73
Zubair Trabzada <zubair.trabzada@gmail.com>
2017-11-28 00:44:43 -0800

    initial commit with basic functionality setup

---
+++ app.js
@@ -0,0 +1,145 @@
+
+// BUDGET CONTROLLER - everything inside these anonymously invoked functions are private unless it is inside return method
+var budgetController = (function() {
+
+    // function constructor for expense
+    var Expense = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    // function constructor for income
+    var Income = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    
+    var data = {
+        allItems: {
+            exp: [],
+            inc: []
+        }, 
+        totals : {
+            exp: 0,
+            inc: 0
+        }
+    };
+
+    return {
+        addItem: function(type, des, val) {
+            var newItem, ID;
+
+            // create new ID
+            if (data.allItems[type].length > 0) {
+                // ID = last ID (array.lenght - 1) in the array + 1 
+                ID = data.allItems[type][data.allItems[type].length - 1].id + 1;   
+            } else {
+                ID = 0;
+            }    
+
+            // depending if we get inc or exp from input (- or +)
+            if (type === 'exp') {
+                newItem = new Expense(ID, des, val);
+            } else if (type === 'inc') {
+                newItem = new Income(ID, des, val);
+            }
+            
+            // push it into our data structure 
+            data.allItems[type].push(newItem);
+
+            // return the new element 
+            return newItem;
+        },
+
+        testing: function() {
+            console.log(data);
+        }
+
+    };
+
+})();
+
+
+// UI CONTROLLER
+var UIController = (function() {
+    
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    }
+
+    return {
+        getInput: function() {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value, // Will be either inc or exp
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value 
+            };
+        },
+        // expose private to public so others can access them
+        getDOMstrings: function() {
+            return DOMstrings;
+        }
+    };
+
+})();
+
+
+// GLOBAL APP CONTROLLER 
+var controller = (function(budgetCtrl, UICtrl) {
+
+    var setupEventListeners = function() {
+
+        var DOM = UICtrl.getDOMstrings();        
+        
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+        
+            document.addEventListener('keypress', function(event) {
+                if (event.keyCode === 13 || event.which === 13) {
+                    ctrlAddItem();
+                }
+            });
+    }
+
+
+    var ctrlAddItem = function() {
+        var input, newItem;
+        
+        // 1. Get the field input data
+        input = UICtrl.getInput();
+
+        // 2. Add the item to the budget controller 
+        newItem = budgetController.addItem(input.type, input.description, input.value);
+
+        // 3. Add the item to the UI
+
+        // 4. Calculate the budget
+
+        // 5. Display the bduget on the UI 
+        
+    };
+
+    // so its available outside the private function 
+    return {
+        // As soon as application start run this:
+        init: function() {
+            console.log('Application has started.');
+            setupEventListeners();
+        }
+    }
+
+})(budgetController, UIController);
+
+controller.init();
+
+
+
+
+
+
+
