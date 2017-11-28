5c606d72f16cd32f7fe59c0afa0b53b36fe4a713
Zubair Trabzada <ztrabzada@losaltosca.gov>
2017-11-28 13:24:44 -0800

    updating UI

--- app.js
+++ app.js
@@ -70,7 +70,9 @@ var UIController = (function() {
         inputType: '.add__type',
         inputDescription: '.add__description',
         inputValue: '.add__value',
-        inputBtn: '.add__btn'
+        inputBtn: '.add__btn',
+        incomeContainer: '.income__list',
+        expensesContainer: '.expenses__list' 
     }
 
     return {
@@ -81,6 +83,34 @@ var UIController = (function() {
                 value: document.querySelector(DOMstrings.inputValue).value 
             };
         },
+
+        addListItem: function(obj, type) {
+            var html, newHtml, element;
+            // Create HTML string with placeholder text
+
+            if (type === 'inc') {
+                element = DOMstrings.incomeContainer;
+                html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+            } else if (type === 'exp') {
+                element = DOMstrings.expesesContainer;   
+                html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+            }
+
+
+            // Replace the placeholder text with some actuall data 
+            newHtml = html.replace('%id%', obj.id);
+            newHtml = newHtml.replace('%description%', obj.description);
+            newHtml = newHtml.replace('%value%', obj.value);            
+
+            // Insert the HTML into the DOM
+            document.querySelector(element).insertAdjacentHTML('beforeend', newHtml); //beforeend - insert as the last child of the list
+        },
+        
+        clearFields: function() {
+            var fields;
+            fields = document.querySelectorAll(DOMstrings.inputDescription + ', ' + DOMstrings.inputValue);    
+        },
+
         // expose private to public so others can access them
         getDOMstrings: function() {
             return DOMstrings;
@@ -117,6 +147,7 @@ var controller = (function(budgetCtrl, UICtrl) {
         newItem = budgetController.addItem(input.type, input.description, input.value);
 
         // 3. Add the item to the UI
+        UICtrl.addListItem(newItem, input.type);
 
         // 4. Calculate the budget
 
