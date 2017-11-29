d36051a9e15f9e71c0f553871634d3a21e28fa54
Zubair Trabzada <ztrabzada@losaltosca.gov>
2017-11-29 11:07:17 -0800

    updating budget calculations functions

--- app.js
+++ app.js
@@ -16,7 +16,15 @@ var budgetController = (function() {
         this.value = value;
     };
 
-    
+    var calculateTotal = function(type) {
+        var sum = 0;
+        data.allItems[type].forEach(function(cur) {
+            sum += cur.value;
+        });
+        data.totals[type] = sum;
+    };
+
+    // our global data structure
     var data = {
         allItems: {
             exp: [],
@@ -25,7 +33,10 @@ var budgetController = (function() {
         totals : {
             exp: 0,
             inc: 0
-        }
+        },
+        budget: 0, 
+        percentage: -1 // -1 is used if nothing exists at the moment
+
     };
 
     return {
@@ -54,6 +65,32 @@ var budgetController = (function() {
             return newItem;
         },
 
+        calculateBudget: function() {
+
+            // calculate total income and expenses 
+            calculateTotal('exp');
+            calculateTotal('inc');
+
+            // calculate the budget: income - expenses 
+            data.budget = data.totals.inc - data.totals.exp;
+
+            // calculate the percentage of income that we spent 
+            if(data.totals.inc > 0) {
+                data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100);
+            } else{
+                data.percentage = -1;
+            }
+        },
+
+        getBudget: function() {
+            return {
+                budget: data.budget,
+                totalInc: data.totals.inc,
+                totalExp: data.totals.exp,
+                percentage: data.percentage
+            };
+        },
+
         testing: function() {
             console.log(data);
         }
@@ -80,7 +117,7 @@ var UIController = (function() {
             return {
                 type: document.querySelector(DOMstrings.inputType).value, // Will be either inc or exp
                 description: document.querySelector(DOMstrings.inputDescription).value,
-                value: document.querySelector(DOMstrings.inputValue).value 
+                value: parseFloat(document.querySelector(DOMstrings.inputValue).value) // parseFloat changes string to a floating number
             };
         },
 
@@ -107,8 +144,17 @@ var UIController = (function() {
         },
         
         clearFields: function() {
-            var fields;
+            var fields, fieldsArr;
             fields = document.querySelectorAll(DOMstrings.inputDescription + ', ' + DOMstrings.inputValue);    
+            // trick javascript using slice method to think fields is an array using Array.prototype
+            fieldsArr = Array.prototype.slice.call(fields);
+
+            fieldsArr.forEach(function(current, index, array) {
+                current.value = "";
+            });
+            // Put focus/pointer (using focus method) at the description input field as soon as its cleared
+            fieldsArr[0].focus();
+
         },
 
         // expose private to public so others can access them
@@ -136,23 +182,40 @@ var controller = (function(budgetCtrl, UICtrl) {
             });
     }
 
+    var updateBudget = function() {
+
+        // 1. Calculate the budget
+        budgetCtrl.calculateBudget();
+
+        // 2. Return the budget
+        var budget = budgetCtrl.getBudget();
+
+        // 3. Display the bduget on the UI 
+        console.log(budget);
+    };
 
     var ctrlAddItem = function() {
         var input, newItem;
         
         // 1. Get the field input data
         input = UICtrl.getInput();
-
-        // 2. Add the item to the budget controller 
-        newItem = budgetController.addItem(input.type, input.description, input.value);
-
-        // 3. Add the item to the UI
-        UICtrl.addListItem(newItem, input.type);
-
-        // 4. Calculate the budget
-
-        // 5. Display the bduget on the UI 
         
+        // make sure descript values are NOT empty and input value is Not a NaN (not not a number/not empty) and input value is > 0
+        if (input.description !== "" && !isNaN(input.value) && input.value > 0) {
+
+            // 2. Add the item to the budget controller 
+            newItem = budgetController.addItem(input.type, input.description, input.value);
+
+            // 3. Add the item to the UI
+            UICtrl.addListItem(newItem, input.type);
+
+            // 4. Clear fields 
+            UICtrl.clearFields();
+            
+            // 5. Calculate and update budget
+            updateBudget();
+
+        }
     };
 
     // so its available outside the private function 
